33dcad4a591f66ad14dd6fe55c931cd156292200
mohits-git <mohitkhatri2805@gmail.com>
2025-10-11 13:52:36 +0530

    fix: update eslint to allow self closing tags

--- eslint.config.js
+++ eslint.config.js
@@ -38,6 +38,8 @@ module.exports = tseslint.config(
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/prefer-self-closing-tags": "error"
+    },
   }
 );
