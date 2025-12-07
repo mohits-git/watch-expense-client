ca821eb748be6556cc4d74c02684155d74a7b1ca
mohits-git <mohitkhatri2805@gmail.com>
2025-12-07 18:37:18 +0530

    feat: add deployement script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+echo "Building production assets..."
+ng build --configuration production
+
+echo "Deleting old files in the s3 bucket..."
+aws s3 rm s3://watchexpense.mohits.me --recursive
+
+echo "Uploading new files to the s3 bucket..."
+aws s3 cp ./dist/watch-expense-client/browser s3://watchexpense.mohits.me --recursive
+
+echo "Invalidating CloudFront cache..."
+aws cloudfront create-invalidation --distribution-id EJ32DRF1SA3HO
+
+echo "Deployment completed successfully!"
+echo "Run 'aws cloudfront list-invalidations --distribution-id EJ32DRF1SA3HO' to check the status of the invalidation."
