945c0d00e870732b4e43192a18ce6b75b22bf3d2
mohits-git <mohitkhatri2805@gmail.com>
2025-12-08 04:14:06 +0000

    fix: add cloudfront invalidation paths to deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "Uploading new files to the s3 bucket..."
 aws s3 cp ./dist/watch-expense-client/browser s3://watchexpense.mohits.me --recursive
 
 echo "Invalidating CloudFront cache..."
-aws cloudfront create-invalidation --distribution-id EJ32DRF1SA3HO
+aws cloudfront create-invalidation --distribution-id EJ32DRF1SA3HO --paths "/*"
 
 echo "Deployment completed successfully!"
 echo "Run 'aws cloudfront list-invalidations --distribution-id EJ32DRF1SA3HO' to check the status of the invalidation."
